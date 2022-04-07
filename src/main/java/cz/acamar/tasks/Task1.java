47f131834e5fa6c7a8b579bce87ff8baafdcc32e
Alex <makcar96@gmail.com>
2022-04-08 00:24:40 +0300

    Init commit

---
+++ src/main/java/cz/acamar/tasks/Task1.java
@@ -0,0 +1,26 @@
+package cz.acamar.tasks;
+
+import org.apache.commons.lang.StringUtils;
+
+public class Task1 {
+
+    /**
+     * Task 1. Length of the last word.
+     * <p>
+     * Given a string s consisting of some words separated by some number of spaces,
+     * return the length of the last word in the string.
+     * <p>
+     * A word is a maximal substring consisting of non-space characters only.
+     * <p>
+     * Example input: "Hello World"
+     * Expected output: 5
+     *
+     * @param str - a string s consisting of some words separated by some number of spaces.
+     * @return - the length of the last word in the string.
+     */
+    public int lengthOfLastWord(final String str) {
+        final String[] words = StringUtils.split(str, " ");
+
+        return words[words.length - 1].length();
+    }
+}
